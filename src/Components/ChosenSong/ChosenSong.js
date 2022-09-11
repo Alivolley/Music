d8cbe595b10135ee6e2a93d23d40d06e0e9c0db4
Ali <alicryptovolley@gmail.com>
2022-09-11 17:40:44 +0430

    chosen song page finished and responsived

--- src/Components/ChosenSong/ChosenSong.js
+++ src/Components/ChosenSong/ChosenSong.js
@@ -1,42 +1,118 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./ChosenSong.css";
 import { FaVolumeUp } from "react-icons/fa";
 import { RiArrowGoBackLine, RiArrowGoForwardFill } from "react-icons/ri";
 import { BsFillPauseFill, BsFillPlayFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import { AiFillPlayCircle } from "react-icons/ai";
 
 export default function ChosenSong() {
+   const chosenSong = useRef();
+   const chosenSongRange = useRef();
+   const volumeRange = useRef();
+   const [songDuration, setSongDuration] = useState(0);
+   const [songCurrent, setSongCurrent] = useState(0);
+   const [songVolume, setSongVolume] = useState(80);
+
+   useEffect(() => {
+      chosenSongRange.current.style.setProperty("--completed-area", `${(chosenSongRange.current.value / songDuration) * 100}%`);
+   }, [songCurrent, songDuration]);
+
+   useEffect(() => {
+      volumeRange.current.style.setProperty("--completed-vol", `${volumeRange.current.value}%`);
+      chosenSong.current.volume = songVolume / 100;
+   }, [songVolume]);
+
+   const songTimeHandler = (time) => {
+      let minute = Math.floor(time / 60);
+      let returnMinute = minute < 10 ? `0${minute}` : minute;
+      let second = Math.floor(time % 60);
+      let returnSecond = second < 10 ? `0${second}` : second;
+      return `${returnMinute}:${returnSecond}`;
+   };
+
+   const onloadHandler = () => {
+      setSongDuration(Math.floor(chosenSong.current.duration));
+      chosenSong.current.volume = songVolume / 100;
+   };
+
+   const songPlayHandler = () => {
+      chosenSong.current.volume = songVolume / 100;
+      setInterval(() => {
+         setSongCurrent(chosenSong.current.currentTime);
+      }, 1000);
+   };
+
+   const rangeChangeHandler = (e) => {
+      chosenSongRange.current.style.setProperty("--any", `${(e.target.value / songDuration) * 100}%`);
+      chosenSong.current.currentTime = e.target.value;
+      setSongCurrent(e.target.value);
+   };
+
+   const nextPervHandler = (val) => {
+      if (val === "prev") {
+         chosenSong.current.currentTime = Number(chosenSong.current.currentTime) - 10;
+         setSongCurrent(chosenSong.current.currentTime);
+      } else {
+         chosenSong.current.currentTime = Number(chosenSong.current.currentTime) + 10;
+         setSongCurrent(chosenSong.current.currentTime);
+      }
+   };
+
    return (
       <div className="chosensong container">
          <div className="row">
             <div className="col-12 col-lg-8">
-               <div className="chosensong-song">
+               <div
+                  className="chosensong-song"
+                  style={{ backgroundImage: "linear-gradient(90deg, rgba(13, 13, 13, 0.96) 36%, rgba(13, 13, 13, 0.363) 100%),url(/pics/taylor.jpg)" }}
+               >
                   <p className="chosensong-song__tiltle">Gorgeous</p>
                   <p className="chosensong-song__artist">Artist : Taylor Swift</p>
                   <p className="chosensong-song__album">Album : Reputation</p>
                </div>
                <div className="chosensong-song__lenght">
-                  <p className="chosensong-song__lenght-start">00:00</p>
-                  <input type="range" className="chosensong-song__range" min="0" max="100" defaultValue="0" />
-                  <p className="chosensong-song__lenght-now">03:45</p>
+                  <p className="chosensong-song__lenght-start">{songTimeHandler(songCurrent)}</p>
+                  <audio src="/gorgeous.mp3" ref={chosenSong} onLoadedMetadata={onloadHandler} onPlay={songPlayHandler}></audio>
+                  <input
+                     type="range"
+                     className="chosensong-song__range"
+                     ref={chosenSongRange}
+                     min="0"
+                     max={songDuration}
+                     value={songCurrent}
+                     onChange={(e) => rangeChangeHandler(e)}
+                  />
+                  <p className="chosensong-song__lenght-now">{songTimeHandler(songDuration)} </p>
                </div>
                <div className="chosensong-contolers">
-                  <button className="contolers-prev">
+                  <button className="contolers-prev" onClick={() => nextPervHandler("prev")}>
                      <RiArrowGoBackLine />
                   </button>
-                  <button className="contolers-pause">
+                  <button
+                     className="contolers-pause"
+                     onClick={() => {
+                        chosenSong.current.pause();
+                     }}
+                  >
                      <BsFillPauseFill />
                   </button>
-                  <button className="contolers-play">
+                  <button className="contolers-play" onClick={() => chosenSong.current.play()}>
                      <BsFillPlayFill />
                   </button>
-                  <button className="contolers-next">
+                  <button className="contolers-next" onClick={() => nextPervHandler("next")}>
                      <RiArrowGoForwardFill />
                   </button>
                   <div className="contolers-volume">
                      <FaVolumeUp className="contolers-volume-icon" />
-                     <input type="range" className="contolers-volume-range" min="0" max="100" defaultValue="100" />
+                     <input
+                        type="range"
+                        ref={volumeRange}
+                        className="contolers-volume-range"
+                        min="0"
+                        max="100"
+                        value={songVolume}
+                        onInput={(e) => setSongVolume(e.target.value)}
+                     />
                   </div>
                </div>
             </div>
